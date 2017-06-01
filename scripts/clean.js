38cdb289b5fc767a42df7a9f12147f6be956e5ea
木农 <shiqi.wsq@alipay.com>
2017-06-01 11:11:56 +0800

    publish error fixed

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,5 +1,10 @@
 const fs = require('fs-extra');
 const chalk = require('chalk');
+const package = require('../package.json');
+
+if (package.main !== 'lib/index.min.js') {
+  throw new Error('Please set main entry to "lib/index.min.js".');
+}
 
 console.log(chalk.green('remove lib/index.js'));
 fs.removeSync('./lib/index.js');
