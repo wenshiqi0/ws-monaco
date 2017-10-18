26593e56ad6ebeede92c426f5a3b2c0a0e703daa
木农 <shiqi.wsq@alipay.com>
2017-10-18 11:49:52 +0800

    need abridge or not

--- postinstall.js
+++ postinstall.js
@@ -1,32 +1,12 @@
-const fs  = require('fs');
-const path = require('path');
-const cp = require('child_process');
-const os = require('os');
-
-const absolute = path.join(__dirname, './extensions');
-
-fs.readdirSync(absolute).forEach(dir => {
-  if (fs.statSync(path.join(absolute, dir)).isDirectory()) {
-    const ret = cp.spawnSync(`npm${os.platform() === 'win32' ? '.cmd' : ''}`, ['i'], {
-      cwd: path.join(__dirname, './extensions', dir)
-    });
-
-    const server = path.join(__dirname, './extensions', dir, 'server');
-    if (fs.existsSync(server)) {
-      const ret = cp.spawnSync(`npm${os.platform() === 'win32' ? '.cmd' : ''}`, ['i'], {
-        cwd: server
-      });
-
-      console.log(ret.stdout.toString('utf-8'));
-    }
-  }
-})
-
 // FIX ME
 const { readFileSync, appendFileSync } = require('fs');
 
-const abridgeTypesPath = require.resolve('antbridge/lib.abridge.d.ts');
-const typescriptEs6 = require.resolve('typescript/lib/lib.es6.d.ts');
-const abridgeTypesText = readFileSync(abridgeTypesPath).toString('utf-8');
+const noAbridge = process.env.NO_ABRIDGE || 0;
 
-appendFileSync(typescriptEs6, abridgeTypesText);
+if (!noAbridge) {
+  const abridgeTypesPath = require.resolve('antbridge/lib.abridge.d.ts');
+  const typescriptEs6 = require.resolve('typescript/lib/lib.es6.d.ts');
+  const abridgeTypesText = readFileSync(abridgeTypesPath).toString('utf-8');
+  
+  appendFileSync(typescriptEs6, abridgeTypesText);
+}
